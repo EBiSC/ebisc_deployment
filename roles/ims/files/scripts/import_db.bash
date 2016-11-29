900712c92efd3b86962e35deaef538a763e85b69
streeter <streeter@ebi.ac.uk>
2016-11-29 18:57:33 +0000

    Better importing of database

--- roles/ims/files/scripts/import_db.bash
+++ roles/ims/files/scripts/import_db.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 docker run --rm \
-  -e PGPASSWORD=${PG_ADMIN_PASSWORD} \
+  -e PGPASSWORD=${PGPASSWORD} \
   -e PGHOST=ims-postgres \
-  -e PGUSER=postgres \
+  -e PGUSER=${PGUSER} \
   -e PGDATABASE=${PGDATABASE} \
   --cap-drop=all \
   --net=ebisc \
   -v /tmp/:/tmp/ \
   ebisc/postgres:latest \
-  sh -c "dropdb -U postgres ${PGDATABASE}; createdb ${PGDATABASE} && gunzip -c /tmp/db.gz | psql -f -"
+  sh -c "gunzip -c /tmp/ebisc.sql.gz | psql -f -"
